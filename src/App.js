74ed39cbaa6989ac843b81373887efb8512c1c78
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-06-27 17:52:34 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Profile from "./components/Profile/Profile";
+import user from './components/Profile/user.json';
+import Statistics from "./components/Statistics/Statistics";
+import statisticalData from "./components/data.json";
+
+
+//главный компонент. один файл, один компонент. пропсы
+const App = () => (
+  <>
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={statisticalData} />
+    <Statistics stats={statisticalData} />;
+ </>
+);
+
+
+//это модуль и из него нужно экспорт компонент
+//и всегда это экспорт по умолчанию
+export default App;
+
+
+
---
+++ src/index.js
@@ -0,0 +1,22 @@
+//пакет react
+//уже не нужен в 17 версии React. Теперь используем jsx.
+import React from "react";
+
+//пакет react-dom рендерит react-элементы в настоящие DOM-дерево
+import ReactDOM from "react-dom"; // есть метод ReactDOM.render /метод рендерит компонент App 
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// 2 вар метод рендерит компонент App 
+// ReactDOM.render(<App />, document.querySelector('#root'));
+
+//<React.StrictMode>  это компонент оборачиваетвсе приложение в строгий режим 
+
+
